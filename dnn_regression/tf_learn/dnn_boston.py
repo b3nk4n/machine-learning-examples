8293a2441ad852c9928c1d58138d65beb42d86ca
bsautermeister <apps@bsautermeister.de>
2017-04-15 11:41:58 +0200

    Added DNN classification example using boston data.

---
+++ dnn_regression/tf_learn/dnn_boston.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pandas as pd
+import tensorflow as tf
+
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+FEATURES = ["crim", "zn", "indus", "nox", "rm",
+            "age", "dis", "tax", "ptratio"]
+LABEL = "medv"
+COLUMNS = FEATURES + [LABEL]
+
+training_set = pd.read_csv("../../data/boston/boston_train.csv", skipinitialspace=True,
+                           skiprows=1, names=COLUMNS)
+test_set = pd.read_csv("../../data/boston/boston_test.csv", skipinitialspace=True,
+                       skiprows=1, names=COLUMNS)
+prediction_set = pd.read_csv("../../data/boston/boston_predict.csv", skipinitialspace=True,
+                             skiprows=1, names=COLUMNS)
+
+feature_cols = [tf.contrib.layers.real_valued_column(k) for k in FEATURES]
+
+regressor = tf.contrib.learn.DNNRegressor(feature_columns=feature_cols,
+                                          hidden_units=[10, 10],
+                                          model_dir="tmp/boston_model",
+                                          activation_fn=tf.nn.relu,
+                                          dropout=0.5,
+                                          optimizer="Adam")
+
+
+def input_fn(data_set):
+    feature_cols_dict = {f: tf.constant(data_set[f].values, shape=[data_set[f].values.shape[0], 1]) for f in FEATURES}
+    labels = tf.constant(data_set[LABEL].values)
+    return feature_cols_dict, labels
+
+print("Training...")
+regressor.fit(input_fn=lambda: input_fn(training_set), steps=1000)
+
+print("Eval...")
+ev = regressor.evaluate(input_fn=lambda: input_fn(test_set), steps=1)
+print("Evaluation: ", ev)
+
+loss_score = ev["loss"]
+print("Loss: {0:f}".format(loss_score))
+
+print("Predictions...")
+predict_result_list = regressor.predict(input_fn=lambda: input_fn(prediction_set))
+
+predictions = list(predict_result_list)
+print ("Predictions: {}".format(str(predictions)))

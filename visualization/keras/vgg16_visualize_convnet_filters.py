6b2dc95f097a00362f2827d1d34feb5c23f2246a
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2018-04-28 15:58:21 +0200

    Added program params and option for a single image

--- visualization/keras/vgg16_visualize_convnet_filters.py
+++ visualization/keras/vgg16_visualize_convnet_filters.py
@@ -1,13 +1,13 @@
 import argparse
 import sys
-import numpy as np
 
+import matplotlib.pyplot as plt
+import numpy as np
 from tensorflow.contrib.keras import applications
 from tensorflow.contrib.keras import backend as K
 
-import matplotlib.pyplot as plt
-
 EPSILON = 1e-5
+MARGIN = 5
 
 
 def tensor_to_image(x):
@@ -23,7 +23,7 @@ def tensor_to_image(x):
     return x
 
 
-def generate_pattern(model, layer_name, filter_index, step_rate, size=224):
+def generate_pattern(model, layer_name, filter_index, steps, learning_rate, size=224):
     layer_output = model.get_layer(layer_name).output
     loss = K.mean(layer_output[:, :, :, filter_index])
 
@@ -39,10 +39,11 @@ def generate_pattern(model, layer_name, filter_index, step_rate, size=224):
 
     # loss maximization via stochastic gradient descent
     input_img_data = np.random.random((1, size, size, 3)) * 20 + 128  # start from gray image with random noise
-    for i in range(40):
+    for i in range(steps):
         loss_value, grads_value = iterate([input_img_data])
+        print('@{:-4d}: {:.4f}'.format(i, loss_value))
         # gradient ascent: adjust the input image in the direction that maximizes the loss
-        input_img_data += grads_value * step_rate
+        input_img_data += grads_value * learning_rate
 
     img_tensor = input_img_data[0]
     return tensor_to_image(img_tensor)
@@ -52,39 +53,51 @@ def main(_):
     model = applications.VGG16(weights='imagenet',
                                include_top=False)
 
-    n = 8
-    results = np.zeros((n * FLAGS.size + (n - 1) * FLAGS.margin,
-                        n * FLAGS.size + (n - 1) * FLAGS.margin,
-                        3), dtype=np.uint8)
+    n = FLAGS.n_sqrt
 
-    for i in range(n):
-        for j in range(n):
-            filter_img = generate_pattern(model,
-                                          FLAGS.layer_name,
-                                          i + (j * n),
-                                          FLAGS.step_rate,
-                                          FLAGS.size)
-            x_start = i * FLAGS.size + i * FLAGS.margin
-            x_end = x_start + FLAGS.size
-            y_start = j * FLAGS.size + j * FLAGS.margin
-            y_end = y_start + FLAGS.size
-            results[x_start:x_end, y_start:y_end, :] = filter_img
+    if n == 1:
+        result_img = generate_pattern(model,
+                                      FLAGS.layer_name,
+                                      0,
+                                      FLAGS.steps,
+                                      FLAGS.learning_rate,
+                                      FLAGS.size)
+    else:
+        result_img = np.zeros((n * FLAGS.size + (n - 1) * MARGIN,
+                               n * FLAGS.size + (n - 1) * MARGIN,
+                               3), dtype=np.uint8)
 
-    print(results.dtype)
+        for i in range(n):
+            for j in range(n):
+                filter_img = generate_pattern(model,
+                                              FLAGS.layer_name,
+                                              i + (j * n),
+                                              FLAGS.steps,
+                                              FLAGS.learning_rate,
+                                              FLAGS.size)
+                x_start = i * FLAGS.size + i * MARGIN
+                x_end = x_start + FLAGS.size
+                y_start = j * FLAGS.size + j * MARGIN
+                y_end = y_start + FLAGS.size
+                result_img[x_start:x_end, y_start:y_end, :] = filter_img
+
+    print(result_img.dtype)
     plt.figure(figsize=(20, 20))
-    plt.imshow(results)
+    plt.imshow(result_img)
     plt.show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--step_rate', type=float, default=1.0,
+    parser.add_argument('--steps', type=int, default=50,
+                        help='The number of gradient-ascent steps per filter')
+    parser.add_argument('--learning_rate', type=float, default=1.0,
                         help='The step rate of gradient ascent')
     parser.add_argument('--layer_name', type=str, default='block5_conv1',
                         help='The convolution layer name to visualize')
-    parser.add_argument('--size', type=int, default=128,
+    parser.add_argument('--size', type=int, default=512,
                         help='The size of filters to show')
-    parser.add_argument('--margin', type=int, default=5,
-                        help='The margin to use')
+    parser.add_argument('--n_sqrt', type=int, default=1,
+                        help='How may feature maps to compute, where N = (n_sqrt)^2')
     FLAGS, unparsed = parser.parse_known_args()
     main([sys.argv[0]] + unparsed)
